f4a8e7e98dd35199075f0ef9d5aed6d9a0b8f915
Nicolas Ganivet <nicolas@cividesk.com>
2016-02-26 00:59:33 -0700

    StackExchange: minor fixes in queries

--- stackexchange/generate.inc.php
+++ stackexchange/generate.inc.php
@@ -4,16 +4,20 @@ require_once('stackapi.php');
 $queries[] = array(
   'file' => 'stackexchange-info.json',
   'query' => "
-      SELECT " . implode(', ', $se_fields['site']) ."
+      SELECT " . implode(',', $se_fields['site']) ."
         FROM stackexchange_history
-       ORDER BY date DESC
+       ORDER BY ts_created DESC
 	   LIMIT 1
    ",
 );
+$fields = array();
+foreach ($se_fields['site'] as $field) {
+  $fields[] = "MAX($field) AS $field";
+}
 $queries[] = array(
   'file' => 'stackexchange-history.json',
   'query' => "
-      SELECT LEFT(ts_created, 7) AS month, MAX(". implode('), MAX(', $se_fields['site']) . ")
+      SELECT LEFT(ts_created, 7) AS month,". implode(',', $fields) . "
         FROM stackexchange_history
        ORDER BY month ASC
    ",
@@ -21,7 +25,7 @@ $queries[] = array(
 $queries[] = array(
   'file' => 'stackexchange-top-users-by-reputation.json',
   'query' => "
-      SELECT display_name, COALESCE(location, ''), reputation
+      SELECT display_name, COALESCE(location, '') AS location, reputation
         FROM stackexchange_users
        ORDER BY reputation DESC
        LIMIT 100
@@ -30,7 +34,7 @@ $queries[] = array(
 $queries[] = array(
   'file' => 'stackexchange-top-users-by-accept-rate.json',
   'query' => "
-      SELECT display_name, COALESCE(location, ''), accept_rate
+      SELECT display_name, COALESCE(location, '') AS location, accept_rate
         FROM stackexchange_users
        ORDER BY accept_rate DESC
        LIMIT 100
@@ -39,7 +43,7 @@ $queries[] = array(
 $queries[] = array(
   'file' => 'stackexchange-top-users-by-badges.json',
   'query' => "
-      SELECT display_name, COALESCE(location, ''), badges_gold, badges_silver, badges_bronze
+      SELECT display_name, COALESCE(location, '') AS location, badges_gold, badges_silver, badges_bronze
         FROM stackexchange_users
        ORDER BY badges_gold DESC, badges_silver DESC, badges_bronze DESC
        LIMIT 100
